938a112a6f0cb23edbb2a5d94cf8540eff87e78d
Strooper44 <lolprince44150@gmail.com>
2025-05-18 00:38:52 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,14 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+    'handling.meta',
+    'vehicles.meta',
+    'carvariations.meta',
+    -- Ajoute ici d'autres fichiers si tu en as, comme carcols.meta, dlctext.meta, etc.
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+-- Ajoute ici les lignes supplémentaires si tu as d'autres fichiers (ex: CARCOLS_FILE)
+
